21f019811d15237e8bffb48449ea50bdf8190f50
‘Karthik <karthikchandrasekhar03@outlook.com>
2024-03-04 19:35:05 +0530

    Initialized Node project

---
+++ cyscom-landing-page/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Sample from "./Components/Sample"
+function App() {
+
+  return (
+    <>
+     <Sample />
+    </>
+  )
+}
+
+export default App
---
+++ cyscom-landing-page/src/Components/Sample.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sample = () => {
+  return (
+    <div className='text-xl text-blue-700'>Sample Component</div>
+  )
+}
+
+export default Sample
