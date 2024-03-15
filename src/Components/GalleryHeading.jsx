35e9a4040be7f8ba52f47a9b140359052435cd18
Akkilesh-A <akkilalagar05@gmail.com>
2024-03-15 18:13:48 +0530

    picture gallery done

---
+++ src/Components/GalleryHeading.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const GalleryHeading = () => {
+  return (
+    <section className="flex flex-col justify-center text-center p-8">
+      <h1 className="font-bold text-7xl">
+        <span className="text-[#6C6C6C]">Our</span> Events
+      </h1>
+      <p className=" p-4 text-xl text-[#6C6C6C]">
+        Over the years, CYSCOM has conducted a number of exciting and innovative
+        events and com petitions for spreading awareness of cybersecurity.
+      </p>
+    </section>
+  )
+}
+
+export default GalleryHeading
---
+++ src/Components/Pictures.jsx
@@ -0,0 +1,205 @@
+const pictures = [
+    {
+      id: 1,
+      imgURL: 'src/assets/images/events/2020hack/1.jpg',
+      text: '2020HACK',
+    },
+    {
+      id: 2,
+      imgURL: 'src/assets/images/events/2020hack/2.jpg',
+      text: '2020HACK',
+    },
+    {
+      id: 3,
+      imgURL: 'src/assets/images/events/2020hack/3.jpg',
+      text: '2020HACK',
+    },
+    {
+      id: 4,
+      imgURL: 'src/assets/images/events/cyber defenders/IMG_1988.webp',
+      text: 'Cyber Defenders 2022',
+    },
+    {
+      id: 5,
+      imgURL: 'src/assets/images/events/cyber defenders/IMG_2000.webp',
+      text: 'Cyber Defenders 2022',
+    },
+    {
+      id: 6,
+      imgURL: 'src/assets/images/events/cyber defenders/IMG_2002.webp',
+      text: 'Cyber Defenders 2022',
+    },
+    {
+      id: 7,
+      imgURL: 'src/assets/images/events/cyber defenders/IMG_2369.webp',
+      text: 'Cyber Defenders 2022',
+    },
+    {
+      id: 8,
+      imgURL: 'src/assets/images/events/cyber defenders/IMG_2372_cropped.webp',
+      text: 'Cyber Defenders 2022',
+    },
+    {
+      id: 9,
+      imgURL: 'src/assets/images/events/cyber defenders/IMG_2521.webp',
+      text: 'Cyber Defenders 2022',
+    },
+    {
+      id: 10,
+      imgURL: 'src/assets/images/events/cyber defenders/IMG_2958.webp',
+      text: 'Cyber Defenders 2022',
+    },
+    {
+      id: 11,
+      imgURL: 'src/assets/images/events/cyber defenders/IMG_2967.webp',
+      text: 'Cyber Defenders 2022',
+    },
+    {
+      id:12,
+      imgURL:'src/assets/images/events/hackoverflow/1.webp',
+      text:'HackOverflow 2022'
+    },
+    {
+      id:13,
+      imgURL:'src/assets/images/events/hackoverflow/2.webp',
+      text:'HackOverflow 2022'
+    },
+    {
+      id:14,
+      imgURL:'src/assets/images/events/hackoverflow/3.webp',
+      text:'HackOverflow 2022'
+    },
+    {
+      id:15,
+      imgURL:'src/assets/images/events/hackoverflow/4.webp',
+      text:'HackOverflow 2022'
+    },
+    {
+      id:16,
+      imgURL:'src/assets/images/events/icrtac2018/3.JPG',
+      text:'ICRTAC 2018'
+    },
+    {
+      id:17,
+      imgURL:'src/assets/images/events/icrtac2018/4.JPG',
+      text:'ICRTAC 2018'
+    },
+    {
+      id:18,
+      imgURL:'src/assets/images/events/icrtac2018/5.JPG',
+      text:'ICRTAC 2018'
+    },
+    {
+      id:19,
+      imgURL:'src/assets/images/events/icrtac2019/1.JPG',
+      text:'ICRTAC 2019'
+    },
+    {
+      id:20,
+      imgURL:'src/assets/images/events/icrtac2019/2.JPG',
+      text:'ICRTAC 2019'
+    },
+    {
+      id:21,
+      imgURL:'src/assets/images/events/icrtac2019/3.JPG',
+      text:'ICRTAC 2019'
+    },
+    {
+      id:22,
+      imgURL:'src/assets/images/events/inauguration/1.JPG',
+      text:'Inauguration'
+    },
+    {
+      id:23,
+      imgURL:'src/assets/images/events/inauguration/2.JPG',
+      text:'Inauguration'
+    },
+    {
+      id:24,
+      imgURL:'src/assets/images/events/inauguration/3.JPG',
+      text:'Inauguration'
+    },
+    {
+      id:25,
+      imgURL:'src/assets/images/events/inauguration/4.JPG',
+      text:'Inauguration'
+    },
+    {
+      id:26,
+      imgURL:'src/assets/images/events/inauguration/5.JPG',
+      text:'Inauguration'
+    },
+    {
+      id:27,
+      imgURL:'src/assets/images/events/inauguration/6.JPG',
+      text:'Inauguration'
+    },
+    {
+      id:28,
+      imgURL:'src/assets/images/events/opengov/02.jpg',
+      text:'OPENGOV'
+    },
+    {
+      id:29,
+      imgURL:'src/assets/images/events/opengov/03.jpg',
+      text:'OPENGOV'
+    },
+    {
+      id:30,
+      imgURL:'src/assets/images/events/opengov/05.jpg',
+      text:'OPENGOV'
+    },
+    {
+      id:31,
+      imgURL:'src/assets/images/events/tetraflip/1.png',
+      text:'Tetraflip'
+    },
+    {
+      id:32,
+      imgURL:'src/assets/images/events/tetraflip/2.png',
+      text:'Tetraflip'
+    },
+    {
+      id:33,
+      imgURL:'src/assets/images/events/tetraflip/3.png',
+      text:'Tetraflip'
+    },
+    {
+      id:34,
+      imgURL:'src/assets/images/events/TOVC 1.0/1.jpg',
+      text:'TOVC 1.0'
+    },
+    {
+      id:35,
+      imgURL:'src/assets/images/events/TOVC 1.0/2.jpg',
+      text:'TOVC 1.0'
+    },
+    {
+      id:36,
+      imgURL:'src/assets/images/events/WASPCON/1.webp',
+      text:'WASPCON'
+    },
+    {
+      id:37,
+      imgURL:'src/assets/images/events/WASPCON/2.webp',
+      text:'WASPCON'
+    },
+    {
+      id:38,
+      imgURL:'src/assets/images/events/WASPCON/3.webp',
+      text:'WASPCON'
+    },
+    {
+      id:39,
+      imgURL:'src/assets/images/events/WASPCON/4.webp',
+      text:'WASPCON'
+    },
+    {
+      id:40,
+      imgURL:'src/assets/images/events/WASPCON/5.webp',
+      text:'WASPCON'
+    },
+  
+]
+
+export default pictures;
